e08dcab680b787a423bb3012f92b8ed900019b1a
76836 <74463275+76836@users.noreply.github.com>
2025-02-16 18:22:50 -0700

    Add files via upload

---
+++ app-data.js
@@ -0,0 +1,14 @@
+const apps = [
+  {
+    id: 1,
+    name: 'Akari',
+    developer: '76836',
+    category: 'Productivity'
+  },
+  {
+    id: 2,
+    name: 'Lunabot',
+    developer: '76836',
+    category: 'Productivity'
+  }
+];
